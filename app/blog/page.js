415620db49b43fb72ee5b3b8b0ae7ab778a5dfd9
Mohamamd Tahir Lashari <61346951+Mohammadtahirlashari@users.noreply.github.com>
2023-09-12 23:38:01 +0500

    h1 added in pages

--- app/blog/page.js
+++ app/blog/page.js
@@ -9,6 +9,7 @@ const Blog = () => {
   return (
 <Wrapper>
 <div className='grid place-content-center place-items-center h-screen'>
+  <h1 className='font-extrabold text-4xl uppercase'>Latest Blog</h1>
           <h2 className='text-2xl md:text-4xl lg:text-6xl text-indigo-600'>Coming Soon</h2>
         </div>
 </Wrapper>
--- app/contact/page.js
+++ app/contact/page.js
@@ -9,6 +9,7 @@ const Contact = () => {
   return (
     <Wrapper>
         <div className='grid place-content-center place-items-center h-screen'>
+          <h1>Contact Us</h1>
           <h2 className='text-2xl md:text-4xl lg:text-6xl text-indigo-600'>Coming Soon</h2>
         </div>
     </Wrapper>
