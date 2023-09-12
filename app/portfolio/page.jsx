0d24592960db0737b462943d01a8ad76c20bbf76
Mohamamd Tahir Lashari <61346951+Mohammadtahirlashari@users.noreply.github.com>
2023-09-12 23:41:28 +0500

    pages changes

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Portfolio = () => {
   return (
     <div className='grid place-content-center place-items-center h-screen'>
-        <h1>Our Portfolio</h1>
+        <h1 className='font-extrabold text-4xl uppercase'>Our Portfolio</h1>
     <h2 className='text-2xl md:text-4xl lg:text-6xl text-indigo-600'>Coming Soon</h2>
   </div>
   )
