e3db2089849e26e000f6533295dbaf9d6d765941
Mohamamd Tahir Lashari <61346951+Mohammadtahirlashari@users.noreply.github.com>
2023-09-12 23:42:18 +0500

    margin bottom

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Services = () => {
   return (
     <div className='grid place-content-center place-items-center h-screen'>
-        <h1 className='font-extrabold text-4xl uppercase'>Our Services</h1>
+        <h1 className='font-extrabold text-4xl uppercase mb-10'>Our Services</h1>
     <h2 className='text-2xl md:text-4xl lg:text-6xl text-indigo-600'>Coming Soon</h2>
   </div>
   )
